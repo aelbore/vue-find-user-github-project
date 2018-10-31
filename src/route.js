eda6ed9170605e4b6676ba8fcaa221a69a4b03b0
aelbore <aelbore.sg@gmail.com>
2018-10-31 20:47:37 +0800

    user lazy load routes

--- src/route.js
+++ src/route.js
@@ -1,16 +1,16 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import Search from './components/search/search';
-import Readme from './components/readme/readme';
-
 Vue.use(VueRouter);
+
+const ReadmeLazyComponent = () => import('./components/readme/readme.vue');
+const SearchLazyComponent = () => import('./components/search/search.vue');
 
 export default new VueRouter({
   mode: 'history',
   base: __dirname,
   routes: [
-    { path: '/', name: 'search', component: Search },
-    { path: '/readme/:username/:repo', name: 'readme', component: Readme, props: true }
+    { path: '/', name: 'search', component: SearchLazyComponent },
+    { path: '/readme/:username/:repo', name: 'readme', component: ReadmeLazyComponent, props: true }
   ]
 })
